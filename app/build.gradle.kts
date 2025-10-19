fcd8d2e9cf12535ca41d92c2835cd10858752ccb
Rafael Schmitt Wilhelms <chimiasama@gmail.com>
2025-10-19 07:22:50 -0300

    Initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     // plugin de serialization
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.10"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.20"
 }
 
 // Exclui globalmente a versão da JetBrains IntelliJ Annotations duplicada
