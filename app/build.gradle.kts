b20955147854f9ab64fa72e5916e0b433a303302
Rafael Schmitt Wilhelms <chimiasama@gmail.com>
2025-12-03 20:48:00 -0300

    ?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.swadebuilder"
         minSdk = 25
         targetSdk = 36
-        versionCode = 12
-        versionName = "2.1"
+        versionCode = 13
+        versionName = "2.2"
 
 
         ndk {
