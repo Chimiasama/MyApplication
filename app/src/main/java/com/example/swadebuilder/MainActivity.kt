5dae9d81e15220df0e4d533f7ebba201c9972b63
Rafael <chimiasama@gmail.com>
2026-02-03 18:30:17 -0300

    ajuste aa aa com nomes ao lado, remoção

--- app/src/main/java/com/example/swadebuilder/MainActivity.kt
+++ app/src/main/java/com/example/swadebuilder/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
